9a9f1b363f86159acaa184e6e73bc68408358943
Ngô Huy <117896799+huydotcode@users.noreply.github.com>
2025-10-26 23:18:20 +0700

    chore: move globals.css to /styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Be_Vietnam_Pro } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
 
 const beVietnamPro = Be_Vietnam_Pro({
   subsets: ["latin"],
